1391a871baddb0548d7adfede9a9a7709c5d2658
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-07-23 07:04:50 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,34 @@
+class Dog
+
+  attr_accessor :name, :breed
+  attr_reader :id
+
+    def initialize(id: nil , name:, breed:)
+      @id = id
+      @name = name
+      @breed = breed
+    end
+
+    def self.create_table
+      sql =  <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+          )
+          SQL
+      DB[:conn].execute(sql)
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+
+    end
+
+
+end
